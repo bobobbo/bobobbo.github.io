937dc424dc4435263a4b3ee54d4755abd7c86e76
bobobbo <48302947+bobobbo@users.noreply.github.com>
2019-06-13 00:55:51 +0900

    다시제출

--- first/js/script.js
+++ first/js/script.js
@@ -1,17 +1,53 @@
-// console.log("load");
+//console.log("연결");
+console.log("load");
 
-var $a = document.querySelector("#btn-top");
-var $scroll = document.scrollingElement||document.body || document.documentElement;
+//셀렉터 찾기.
+var $cursorDot = document.querySelector("#cursor-dot");
+var $cursorBG = document.querySelector("#cursor-bg");
+var $progress = document.querySelector("#progress");
+var $list = document.querySelector("#section");
+var $listEl = $list.querySelectorAll("a");
 
+//커서 움직이기.
+window.addEventListener("mousemove", onMouseMoveWindow);
 
-$a.addEventListener("click", onclickbtntop);
-function onclickbtntop(e){
-    e.preventDefault();
-    console.log("click btn top");
-    anime({
-        targets : $scroll,
-        scrollTop : 0,
-        duration : 10000,
-        ease : 'easeInOutQuad'
+function onMouseMoveWindow(e) {
+    var posX = e.pageX, posY = e.pageY;
+    //현재 커서의 좌표값.
+    // $cursorDot.style.top = posY + 'px';
+    // $cursorDot.style.left = posX + 'px';
+    TweenMax.killTweensOf($cursorDot);
+    TweenMax.killTweensOf($cursorBG);
+    TweenMax.killTweensOf($progress);
+    TweenMax.to($cursorDot, 0.15, {css: {top: posY, left: posX}});
+    TweenMax.to($cursorBG, 0.2, {css: {top: posY, left: posX}});
+    TweenMax.to($progress, 0.25, {css: {top: posY, left: posX}});
+}
+
+for(var i = 0; i < $listEl.length; i++){
+    // console.log($listEl[i]);
+    $listEl[i].addEventListener('mouseenter', function(){
+        // console.log('enter');
+        if(!$cursorDot.classList.contains("active")){
+            $cursorDot.classList.add("active");
+        }
+        if(!$cursorBG.classList.contains("active")){
+            $cursorBG.classList.add("active");
+        }
+        if(!$progress.classList.contains("active")){
+            $progress.classList.add("active");
+        }
+    })
+    $listEl[i].addEventListener('mouseleave', function(){
+        // console.log('leave');
+        if($cursorDot.classList.contains("active")){
+            $cursorDot.classList.remove("active");
+        }
+        if($cursorBG.classList.contains("active")){
+            $cursorBG.classList.remove("active");
+        }
+        if($progress.classList.contains("active")){
+            $progress.classList.remove("active");
+        }
     });
 }
